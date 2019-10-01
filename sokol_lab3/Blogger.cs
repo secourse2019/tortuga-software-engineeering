b625dd5235a7094e25ae321b1241c0cf688efb17
bezbahen0 <bezbahen0@mail.ru>
2019-10-01 16:18:30 +0300

    add lab3

---
+++ sokol_lab3/Blogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagrammaApp
+{
+    class Blogger : Worker
+    {
+        public Blogger() { }
+
+        public void SheckPayment() { }
+
+        public void ClaplnTheAss(Passager passager) { }
+    }
+}
---
+++ sokol_lab3/Subway.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagrammaApp
+{
+    class Subway : UndergroundTransport
+    {
+        public List<Passager> Passagers { get; set; }
+
+        public Subway() { }
+    }
+}
